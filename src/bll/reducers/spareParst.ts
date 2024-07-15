47307d265f3868b475fd6059b78b47071f28d780
DimaMaslo88 <dmitrij-maslo@mail.ru>
2024-07-15 21:46:56 +0300

    "feat/bll logic"

--- src/bll/reducers/spareParst.ts
+++ src/bll/reducers/spareParst.ts
@@ -1,4 +1,4 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {sparePartsStateType} from "types/sparePartsType";
 
 
@@ -6,5 +6,14 @@ const sparePartsState = [] as sparePartsStateType[]
 
 const slice = createSlice({
     name: 'spareParts',
+    initialState: sparePartsState,
+    reducers: {
+        setSpareParts(state, action: PayloadAction<{ value: sparePartsStateType }>) {
+            state.push({...action.payload.value})
+        }
+    }
+
 
 })
+
+export const sparePartsReducer = slice.reducer
--- src/bll/store.ts
+++ src/bll/store.ts
@@ -1,14 +1,20 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import thunkMiddleware from "redux-thunk"
+import { sparePartsReducer} from "./reducers/spareParst";
 
-export const store = configureStore({
-    reducer:{
-        spareParts,
-    },
-    middleware:(getDefaultMiddleware) =>
-        getDefaultMiddleware()
-            .prepend(thunkMiddleware),
-
-
-    devTools:true
+const rootReducer = combineReducers({
+    spareParts:sparePartsReducer
 })
+export const store = configureStore({
+    reducer:rootReducer,
+    // middleware:(getDefaultMiddleware) =>
+    //     getDefaultMiddleware()
+    //         .prepend(thunkMiddleware),
+
+})
+export type RootReducerType = ReturnType<typeof rootReducer>
+
+// @ts-ignore
+window.store = store
+
+
