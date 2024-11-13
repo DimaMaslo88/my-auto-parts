bca575d2385feb7b9dad8488336118e5fe6bf388
DimaMaslo88 <dmitrij-maslo@mail.ru>
2024-11-13 19:55:50 +0300

    "feat/main page"

--- src/ui/pages/mainPage/MainPage.tsx
+++ src/ui/pages/mainPage/MainPage.tsx
@@ -5,6 +5,7 @@ import {GetUserCar} from "bll/slices/userCars";
 import {useAppDispatch} from "utils/hooks";
 import {selectUserCars} from "bll/selectors";
 import {useSelector} from "react-redux";
+import {UserCarsTypes} from "types/userCarsTypes";
 
 
 
@@ -19,12 +20,15 @@ dispatch(GetUserCar())
     return (
         <div className={style.mainPageContainer}>
             <div>
-                <ul className={style.ul}>
-                    <li className={style.li}> Название авто</li>
-                    <li className={style.li}> Год Выпуска</li>
-                    <li className={style.li}> Тип Двигателя</li>
-                    <li className={style.li}> Привод</li>
-                </ul>
+                {userCars.map(({ id,car,model,generation,year}:UserCarsTypes)=>(
+                    <ul className={style.ul} key={id}>
+                        <li className={style.li}> Название авто  {car}</li>
+                        <li className={style.li}> Модель  {model}</li>
+                        <li className={style.li}> Поколение {generation}</li>
+                        <li className={style.li}> Год выпуска {year} </li>
+                    </ul>
+                ))}
+
 
             </div>
             <img src={img} alt='ford explorer 5' className={style.image}/>
