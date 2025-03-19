4875b35c89c6d3ed852496aaf549549a4520a323
DimaMaslo88 <dmitrij-maslo@mail.ru>
2025-03-19 20:55:59 +0300

    "feat/authPage"

--- src/ui/components/authorization/Authorization.tsx
+++ src/ui/components/authorization/Authorization.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import style  from 'styles/authorization/authorization.module.scss'
+import style from 'styles/authorization/authorization.module.scss'
 
 export const Authorization = () => {
     return (
+        // в компоненте будет использована библиотека formik для веритификации
         <div className={style.authContainer}>
-            Hello
+            В процессе разработки...... :)))
+
         </div>
     );
 };
